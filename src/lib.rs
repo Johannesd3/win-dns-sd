e66c69adb75d15cad5aff188939b3234d5b0b3d7
johannesd3 <johannesd@outlook.com>
2021-05-13 12:47:39 +0200

    Improve api

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,14 @@ mod bindings {
 }
 
 use std::convert::TryFrom;
-use std::fmt;
 
-use bindings::Windows::Networking::{
-    HostName as WinHostName, ServiceDiscovery::Dnssd::DnssdServiceInstance, Sockets::DatagramSocket,
+use thiserror::Error;
+
+use self::bindings::Windows::Networking::{
+    HostName as WinHostName,
+    ServiceDiscovery::Dnssd::{DnssdRegistrationStatus, DnssdServiceInstance},
+    Sockets::DatagramSocket,
 };
-
-async fn register(
-    name: &str,
-    hostname: Option<WinHostName>,
-    port: u16,
-    txt: &[(&str, &str)],
-) -> windows::Result<(DnssdServiceInstance, DatagramSocket)> {
-    let instance = DnssdServiceInstance::Create(name, hostname, port)?;
-
-    let txt_map = instance.TextAttributes()?;
-    for &(key, value) in txt {
-        txt_map.Insert(key, value)?;
-    }
-
-    let socket = DatagramSocket::new()?;
-    instance.RegisterDatagramSocketAsync1(&socket)?.await?;
-
-    Ok((instance, socket))
-}
 
 pub struct Hostname(WinHostName);
 
@@ -36,26 +20,62 @@ impl TryFrom<&str> for Hostname {
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         WinHostName::CreateHostName(value)
             .map(Hostname)
+            .map_err(ErrorInner::WindowsError)
             .map_err(Error)
     }
 }
 
-#[derive(Debug)]
-pub struct Error(windows::Error);
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.0.fmt(f)
-    }
+#[derive(Debug, Error)]
+enum ErrorInner {
+    #[error(transparent)]
+    WindowsError(#[from] windows::Error),
+    #[error("Cannot register service: Invalid service name")]
+    InvalidServiceName,
+    #[error("Cannot register service: Server error")]
+    ServerError,
+    #[error("Cannot register service: Security error")]
+    SecurityError,
+    #[error("Cannot register service: Unknown error")]
+    UnkownError,
 }
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(&self.0)
+#[derive(Debug, Error)]
+#[error(transparent)]
+pub struct Error(ErrorInner);
+
+type ServiceInner = (DnssdServiceInstance, DatagramSocket);
+
+async fn register(
+    name: &str,
+    hostname: Option<WinHostName>,
+    port: u16,
+    txt: &[(&str, &str)],
+) -> Result<ServiceInner, ErrorInner> {
+    let instance = DnssdServiceInstance::Create(name, hostname, port)?;
+
+    let txt_map = instance.TextAttributes()?;
+    for &(key, value) in txt {
+        txt_map.Insert(key, value)?;
     }
+
+    let socket = DatagramSocket::new()?;
+    let result = instance
+        .RegisterDatagramSocketAsync1(&socket)?
+        .await?
+        .Status()?;
+
+    match result {
+        DnssdRegistrationStatus::Success => (),
+        DnssdRegistrationStatus::InvalidServiceName => return Err(ErrorInner::InvalidServiceName),
+        DnssdRegistrationStatus::SecurityError => return Err(ErrorInner::SecurityError),
+        DnssdRegistrationStatus::ServerError => return Err(ErrorInner::ServerError),
+        _ => return Err(ErrorInner::UnkownError),
+    }
+
+    Ok((instance, socket))
 }
 
-pub struct Service((DnssdServiceInstance, DatagramSocket));
+pub struct Service(ServiceInner);
 
 impl Service {
     pub async fn register(
